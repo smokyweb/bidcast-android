c098d19ae033d4736a1e896bb919c79116918bd8
Blue Mini <bluemini@Blues-Mac-mini.local>
2026-05-13 14:25:06 -0400

    Remove Bidcast logo from app screens (task cmp4936rn00m73mx1nzx1a5fu)

--- app/src/main/java/io/bidswipe/app/ui/sellerHub/PremierShopFragment.kt
+++ app/src/main/java/io/bidswipe/app/ui/sellerHub/PremierShopFragment.kt
@@ -125,7 +125,7 @@ class PremierShopFragment : BaseFragment<SellerHubViewModel, FragmentPremierShop
 					// QA-FIX: delivery progress was using responseProg by mistake
 					bind.deliveryProgress.progress = deliveryProg
 
-					bind.reviewLogo.loadUrl(mCtx, mData?.reviewLogo ?: "")
+					bind.reviewLogo.visibility = View.GONE
 
 					bind.reviewTitle.text = mData?.reviewTitle
 //					bind.policyStanding.text = mData?.
